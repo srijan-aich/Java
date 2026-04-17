cc873235f551b20dc34909f9503047cf2ff33b83
srijan-aich <srijanaich22@gmail.com>
2026-04-17 14:39:21 +0000

    learned datatypes

---
+++ variables_data-types/datatypes1.java
@@ -0,0 +1,18 @@
+class datatypes1 {
+    public static void main(String a[]) 
+    {
+
+        int x = 10; // integer data type
+        double y = 20.5; // double data type
+        char z = 'A'; // character data type
+        boolean isTrue = true; // boolean data type
+
+        byte b = 100; // byte data type
+        short s = 1000; // short data type
+        long l = 100000L; // long data type
+
+        float f = 10.5f; // float data type
+
+    } 
+
+}
