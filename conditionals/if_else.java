b9906077b7953affea6f765fdb9a4bbece310756
srijan-aich <srijanaich22@gmail.com>
2026-04-18 13:55:02 +0000

    learned if-else

---
+++ conditionals/if_else.java
@@ -0,0 +1,16 @@
+public class if_else {
+    public static void main(String args[]) {
+        
+        int num = 10;
+        int num1 = 20; 
+
+        // 1. Simple if-else statement
+        if (num < num1) {
+            System.out.println(num);
+        }
+        else {
+            System.out.println(num1);
+        }
+
+    }
+}
