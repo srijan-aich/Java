76d9e09f89786935beab4521d4217669a1ad6e52
srijan-aich <srijanaich22@gmail.com>
2026-04-19 11:35:36 +0000

    add do while

---
+++ loops/dowhile.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String a[]) {
+        
+        int i = 5;
+
+        do {
+            System.out.println("Hi " + i);
+            i++;
+        } while (i <= 4); 
+        
+    }
+}
