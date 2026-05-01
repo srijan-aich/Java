907671c4e174c44fd867174c835fce7fa90eb13a
srijan-aich <srijanaich22@gmail.com>
2026-05-01 14:49:34 +0000

    add arrays

---
+++ arrays/array1.java
@@ -0,0 +1,29 @@
+public class array1 {
+    public static void main(String[] args) {
+
+        //int nums[] = {1, 2, 3, 4, 5}; Declare and initialize an array of integers
+        //nums[0] = 10; // change the value of the first element of the array
+        //System.out.println("nums[0]: " + nums[0]); print the value of the first element of the array 
+        // first element is 10 because we changed it from 1 to 10...
+
+
+
+
+        int[] arr = new int[5]; // Declare an array of integers with size 5
+
+        // Initialize the array with values
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+
+        // Print the elements of the array
+        System.out.println("arr[0]: " + arr[0]);
+        System.out.println("arr[1]: " + arr[1]);
+        System.out.println("arr[2]: " + arr[2]);
+        System.out.println("arr[3]: " + arr[3]); 
+        System.out.println("arr[4]: " + arr[4]);
+       
+    }
+}
