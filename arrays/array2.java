54e02bf061a257a293b3a15916f086d0db079eb5
srijan-aich <srijanaich22@gmail.com>
2026-05-02 03:32:32 +0000

    printed array using loop

---
+++ arrays/array2.java
@@ -0,0 +1,14 @@
+public class array2 {
+    public static void main(String [] args){
+        int arr[] = new int[5]; // Declare an array of integers with size 5
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+
+        for(int i = 0; i<5; i++){
+            System.out.println(arr[i]);
+        }
+    }
+} 
