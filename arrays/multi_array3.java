c9e9189f188e601162ac610f68e07d681375239d
srijan-aich <srijanaich22@gmail.com>
2026-05-02 06:11:02 +0000

    add multi dim arrays

---
+++ arrays/multi_array3.java
@@ -0,0 +1,25 @@
+public class multi_array3 {
+    public static void main(String[] args) {
+        int nums[][] = new int[3][4]; // Declare a 2D array of integers with 3 rows and 4 columns
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 4; j++) {
+                nums[i][j] = (int)(Math.random() * 100); // Initialize the 2D array with random integers between 0 and 99
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 4; j++) {
+                System.out.print(nums[i][j] + " "); // Print the elements of the 2D array
+            }
+            System.out.println(); // Print a new line after each row
+        }
+
+        for(int n[] : nums) { // Use a for-each loop to iterate through the 2D array
+            for(int m : n) {
+                System.out.print(m + " "); // Print each element in the current row
+            }
+            System.out.println(); // Print a new line after each row
+        }
+    }
+}
