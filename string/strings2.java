daa977942c0fd6066d94e33330142ecea5bdc88e
srijan-aich <srijanaich22@gmail.com>
2026-06-08 04:51:35 +0000

    String Buffer/Builder add

--- string/strings2.java
+++ string/strings2.java
@@ -1,4 +1,7 @@
-// String-buffer 
+// String-buffer and builder are mutable, so they can be modified without creating a new string object.
+// builder and buffer are similar
+// builder is not thread safe, so it is faster than buffer, which is thread safe. Therefore, builder should be used when thread safety is not a concern, and buffer should be used when thread safety is a concern.
+
 class strings2 {
     public static void main(String[] args) {
 
@@ -10,5 +13,12 @@ class strings2 {
         sb.deleteCharAt(4); //deletes the character at index 4 (the 'a' in "Srijan")
         String str = sb.toString(); //converts the StringBuffer to a String
         System.out.println(str); //prints the string after deletion
+
+        /*StringBuilder sb2 = new StringBuilder("Srijan");
+        sb2.append(" Aich"); //StringBuilder is also mutable, so the same string object is modified
+        System.out.println(sb2.length()); //prints the length of the string in the StringBuilder
+        System.out.println(sb2.capacity()); //prints the capacity of the StringBuilder
+        System.out.println(sb2); //prints the string in the StringBuilder*/
+
     }
 }
