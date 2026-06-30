cbf1dbe247c96579c791e2f0ed01d463b4364487
srijan-aich <srijanaich22@gmail.com>
2026-06-30 15:49:47 +0000

    add nameConvention

---
+++ oops_/nameConvention.java
@@ -0,0 +1,13 @@
+// Name Convention in Java
+
+// class name or interface name - Humans
+// variable name - age, name
+// method name - show(), getAge(), setAge()
+// constant name - PI, BRAND
+
+// java follows camelCase naming 
+
+// showMyMarks() , other lang - show_my_marks() , both works
+// MyData - class name
+
+// Humans() - constructor. 
