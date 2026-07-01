c8064b0e90712751a49b7f6c6aafcd3b52eda3cf
srijan-aich <srijanaich22@gmail.com>
2026-07-01 16:45:52 +0000

    add inheritance

--- oops_/inheritance.java
+++ oops_/inheritance.java
@@ -13,15 +13,18 @@ class Calc{
 
 public class inheritance{
     public static void main(String[] args) {
-        AdvCalc c = new AdvCalc(); // we don't write Calc because it will only give access to Calc methods not AdvCalc methods. AdvCalc is Calc as extends. 
+        VeryAdvCalc c = new VeryAdvCalc(); // we don't write Calc because it will only give access to Calc methods not AdvCalc methods. AdvCalc is Calc as extends. 
         int r1 = c.add(10, 20);
         int r2 = c.sub(20, 10);
         int r3 = c.multi(10, 20);
         int r4 = c.div(20, 10);
+        double r5 = c.pow(4, 2);
 
         System.out.println("Addition : "+r1);
         System.out.println("Subtraction : "+r2);
         System.out.println("Multiplication : "+r3);
         System.out.println("Division : "+r4);
+        System.out.println("Power : "+r5);
+
     }
 }
---
+++ oops_/inheritance3.java
@@ -0,0 +1,12 @@
+class VeryAdvCalc extends AdvCalc{
+
+    public double pow(int n1, int n2){
+
+        return Math.pow(n1, n2);
+    }
+} //multilevel inheritance. 
+
+//javac *.java
+//srijan@SRIJAN-S-PC:~/dev/Java/oops_$ javac inheritance   to run.
+
+// when 3+ multi , when 2 or less then single inheritance. 
