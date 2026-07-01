3cd8f6c95a1461a9d9874c4c4e370a9f50b76bbf
srijan-aich <srijanaich22@gmail.com>
2026-07-01 05:19:13 +0000

    add inheritance

---
+++ oops_/inheritance.java
@@ -0,0 +1,27 @@
+class Calc{
+
+    public int add(int a , int b)
+    {
+        return a+b;
+    }
+
+    public int sub(int a , int b)
+    {
+        return a-b;
+    }
+}
+
+public class inheritance{
+    public static void main(String[] args) {
+        AdvCalc c = new AdvCalc(); // we don't write Calc because it will only give access to Calc methods not AdvCalc methods. AdvCalc is Calc as extends. 
+        int r1 = c.add(10, 20);
+        int r2 = c.sub(20, 10);
+        int r3 = c.multi(10, 20);
+        int r4 = c.div(20, 10);
+
+        System.out.println("Addition : "+r1);
+        System.out.println("Subtraction : "+r2);
+        System.out.println("Multiplication : "+r3);
+        System.out.println("Division : "+r4);
+    }
+}
---
+++ oops_/inheritance2.java
@@ -0,0 +1,17 @@
+class AdvCalc extends Calc{
+
+    public int multi(int a , int b)
+    {
+        return a*b;
+    }
+
+    public int div(int a , int b)
+    {
+        return a/b;
+    }
+}
+
+
+/*javac inheritance.java inheritance2.java
+srijan@SRIJAN-S-PC:~/dev/Java/oops_$ javac *.java
+srijan@SRIJAN-S-PC:~/dev/Java/oops_$ java inheritance*/
