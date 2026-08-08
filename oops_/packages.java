07b1958cc00152f79432326d3129fadea8cb2966
Srijan Aich <srijanaich22@gmail.com>
2026-08-08 19:39:24 +0530

    add packages

---
+++ oops_/packages.java
@@ -0,0 +1,10 @@
+/* I am not gonna use packages because I already have a folder setup
+if I use packages it would make things messy, but I do know packages */
+
+// packages tools; creates tools folder
+// if we put files in tools folder then we can use -  import.tools.filename;   to access those files
+// all files - import tools.*;
+
+//packages can have unique names, so that we can avoid name conflicts
+// package com.example.myapp;  // this is a unique name for a package
+
