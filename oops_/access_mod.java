3dd5b91d45848a1265b021eede8f63aa06c6fc30
srijan-aich <srijanaich22@gmail.com>
2026-08-18 19:06:05 +0530

    add access modifiers

---
+++ oops_/access_mod.java
@@ -0,0 +1,28 @@
+/*public class access_mod {
+    class Student {
+
+    private int age = 20; 
+
+    // cannot access private variable outside the class
+}
+
+Student s = new Student();
+//System.out.println(s.age); // won't work because age is private
+
+//getAge()
+//setAge() // we need to use this as we know.
+
+//public class Student {
+
+    public int age = 20; // anyone can access this variable
+}
+
+//Student s = new Student();
+//System.out.println(s.age); // works because age is public
+
+//class Student {
+
+    int age = 20; // default access modifier, accessible within the same package
+} 
+
+}*/
