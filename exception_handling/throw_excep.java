e23b5e50e11b9610e3743e0bde8c3315a50e4492
srijan-aich <srijanaich22@gmail.com>
2026-08-30 12:24:18 +0530

    add throw keyword use

---
+++ exception_handling/throw_excep.java
@@ -0,0 +1,23 @@
+public class throw_excep {
+    public static void main(String[] args) {
+        
+        int i = 20;
+        int j = 0;
+
+        try{
+            j = 18/i;
+            if(j==0){
+            throw new ArithmeticException("I don't want to print zero"); //this will print the catch
+            }
+        }
+        catch(ArithmeticException e)
+        {
+            j = 18;
+            System.out.println("Default Output:");
+        }
+
+        System.out.println(j); 
+        System.out.println("bye");
+
+    }
+}
